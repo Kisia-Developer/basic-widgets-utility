1d35ef98be38c2369472ab2488f724e9521b4caa
Kisia-Developer <med4ex.pyo@gmail.com>
2022-05-15 14:40:56 +0900

    add dialog page

---
+++ lib/pages/dialog_page.dart
@@ -0,0 +1,189 @@
+// 外側を押しても消える
+// cancelを押さないと消えない
+// ios -> cupertinoAlertDialog
+// customDialog
+// dialogではないが、bottomSheet
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DialogPage extends StatelessWidget {
+  const DialogPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dialog'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Simple Dialog
+            TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: const Text("Simple Dialogを選びます"),
+                      children: <Widget>[
+                        // コンテンツ領域
+                        SimpleDialogOption(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text("Simple Dialog"),
+                        ),
+                        const SimpleDialogOption(
+                          child: Text("Spimle Dialog"),
+                        ),
+                        const SimpleDialogOption(
+                          child: Text("Smplie Dialog"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Text(
+                'Simple Dialog',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // AlertDialog
+            TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Alert Dialog"),
+                      content: const Text("ここにメッセージ詳細"),
+                      actions: <Widget>[
+                        // ボタン領域
+                        TextButton(
+                          child: const Text("Cancel"),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        TextButton(
+                          child: const Text("OK"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                duration: Duration(seconds: 1),
+                                content: Text('OKが押されました'),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                  // ここを追加すると、cancelボタンのみで閉じることができる
+                  barrierDismissible: false,
+                );
+              },
+              child: const Text(
+                'Alert Dialog',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // CupertinoAlertDialog
+            /// CupertinoDialogはDeprecatedではないらしい
+            TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return CupertinoAlertDialog(
+                      title: const Text("Cupertino Alert"),
+                      content: const Text("Cupertinoは地名です"),
+                      actions: <Widget>[
+                        CupertinoDialogAction(
+                          child: const Text("cancel"),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        CupertinoDialogAction(
+                          child: const Text("そうなのね！"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                duration: Duration(seconds: 1),
+                                content: Text('そうなんですよね〜'),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Text(
+                'CupertinoDialog',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            // CupertinoModalPopup : for iOS
+            // modal_bottom_sheetというpackageでも実装可能
+            TextButton(
+              onPressed: () {
+                showCupertinoModalPopup(
+                  context: context,
+                  builder: (BuildContext context) => CupertinoActionSheet(
+                    title: const Text('Choose Options'),
+                    message: const Text('Your options are '),
+                    actions: <Widget>[
+                      CupertinoActionSheetAction(
+                        child: const Text('One'),
+                        onPressed: () {
+                          Navigator.pop(context, 'One');
+                        },
+                      ),
+                      CupertinoActionSheetAction(
+                        child: const Text('Two'),
+                        onPressed: () {
+                          Navigator.pop(context, 'Two');
+                        },
+                      )
+                    ],
+                    cancelButton: CupertinoActionSheetAction(
+                      isDefaultAction: true,
+                      onPressed: () {
+                        Navigator.pop(context, 'Cancel');
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                  ),
+                );
+              },
+              child: const Text(
+                'ActionSheet',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/top_pages.dart
+++ lib/pages/top_pages.dart
@@ -1,5 +1,6 @@
 import 'package:basic_widgets_utility/pages/buttons_page.dart';
 import 'package:basic_widgets_utility/pages/decoration_page.dart';
+import 'package:basic_widgets_utility/pages/dialog_page.dart';
 import 'package:flutter/material.dart';
 
 class TopPage extends StatelessWidget {
@@ -13,60 +14,124 @@ class TopPage extends StatelessWidget {
         title: const Text('Basic Widgets Utility'),
       ),
       // コンテンツの表示
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
         children: [
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const ButtonsPage(),
-              ),
-            ),
-            child: SizedBox(
-              height: 100,
-              width: MediaQuery.of(context).size.width * 0.4,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ButtonsPage(),
+                  ),
                 ),
-                child: const Center(
-                  child: Text(
-                    'Buttons',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                child: SizedBox(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Buttons',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+              GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const DecorationPage(),
+                  ),
+                ),
+                child: SizedBox(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Decoration',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const DecorationPage(),
-              ),
-            ),
-            child: SizedBox(
-              height: 100,
-              width: MediaQuery.of(context).size.width * 0.4,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const DialogPage(),
+                  ),
                 ),
-                child: const Center(
-                  child: Text(
-                    'Decoration',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                child: SizedBox(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Dialogs',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+              GestureDetector(
+                // onTap: () => Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (context) => const DecorationPage(),
+                //   ),
+                // ),
+                child: SizedBox(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'to be next...',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
